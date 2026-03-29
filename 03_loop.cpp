4fc0338a9412575678da4edec1cf8ed79deae001
memon alfaz <memonalfaz749@gmail.com>
2026-03-29 21:43:08 +0530

    Added new files

---
+++ 03_loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+
+    int sum=0,size;
+    cout<<"Enter any number :";
+    cin>>size;
+
+    for(int i=1;i<=size;i++){
+        sum =sum + i;
+    }
+
+    cout<<"additon of 0 to "<<size<<" is "<<sum<<endl;
+
+
+
+
+
+
+    return 0;
+}
